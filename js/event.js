42923a9f1cd953cd599c413c9fdf63a0548c0083
DatApplePy <szohrfelix@outlook.hu>
2022-10-25 16:06:44 +0200

    new functions added

--- js/event.js
+++ js/event.js
@@ -1,4 +1,52 @@
-import { normalBoardState } from "../resource/normallevel.js";
-import { render } from "./renderer.js";
+import {detailsDiv, gameDiv, menuDiv} from "./references.js";
+import {easyBoardState} from "../resource/easylevel.js";
+import {normalBoardState} from "../resource/normallevel.js";
+import {hardBoardState} from "../resource/hardlevel.js";
+import {state} from "./state.js";
+import {render} from "./renderer.js";
 
-window.onload = render(normalBoardState);
+export function loadEasyLevel() {
+    state.loadLevel(easyBoardState);
+    switchPanel();
+    render(state.board);
+}
+
+export function loadNormalLevel() {
+    state.loadLevel(normalBoardState);
+    switchPanel();
+    render(state.board);
+}
+
+export function loadHardLevel() {
+    state.loadLevel(hardBoardState);
+    switchPanel();
+    render(state.board);
+}
+
+function switchPanel() {
+    menuDiv.style.display = "none";
+    gameDiv.style.display = "flex";
+}
+
+export function showDetails() {
+    if(detailsDiv.style.maxHeight) {
+        detailsDiv.style.maxHeight = null;
+    } else {
+        detailsDiv.style.maxHeight = detailsDiv.scrollHeight + "px";
+    }
+}
+
+export function onCellClick(event) {
+    if(!event.target.matches("td")) {
+        return;
+    }
+
+    const cell = event.target;
+    const x = cell.cellIndex;
+    const y = cell.parentNode.rowIndex;
+
+    if(!state.isObstacle(x, y)) {
+        state.putBulb(x, y);
+        render(state.board);
+    }
+}
