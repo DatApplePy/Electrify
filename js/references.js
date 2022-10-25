35426db0fc0faac551176d966d1096c3d9043ebc
DatApplePy <szohrfelix@outlook.hu>
2022-10-25 16:07:11 +0200

    new references added

--- js/references.js
+++ js/references.js
@@ -1,1 +1,15 @@
+export const bulb = `<img src="../resource/light-bulb-idea-svgrepo-com.svg" alt=""/>`
+export const yellow = "#FAAE7B";
+export const red = "#FF7D7D";
+export const green = "#4E9F3D";
+export const white = "#623A73";
+export const black = "#000000";
+export const menuDiv = document.querySelector("#menu");
+export const gameDiv = document.querySelector("#game");
 export const tableElement = document.querySelector("#play-field");
+export const loadEasyButton = document.querySelector("#easy");
+export const loadNormalButton = document.querySelector("#normal");
+export const loadHardButton = document.querySelector("#hard");
+export const loadSavedGameButton = document.querySelector("#saved-game");
+export const detailsButton = document.querySelector("#details-button");
+export const detailsDiv = document.querySelector("#details");
