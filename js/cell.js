9da97f90ebe6870cbffe74a620e85aaa8532f532
Szohr Félix <szohrfelix@outlook.hu>
2022-11-13 18:22:42 +0100

    reset method added

--- js/cell.js
+++ js/cell.js
@@ -1,7 +1,16 @@
+import { dark } from "./references.js";
+
 export class Cell {
     constructor(color, value) {
         this.style = color;
         this.value = value;
+        this.lightSourceCount = 0;
+        this.overlappingBulbs = 0;
+    }
+
+    reset() {
+        this.style = dark;
+        this.value = "";
         this.lightSourceCount = 0;
         this.overlappingBulbs = 0;
     }
